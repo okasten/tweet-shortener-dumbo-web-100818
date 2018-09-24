e1f27f68f9a67f1d3e35aa7fd32d6fbf815c6303
Olivia Kasten <odkasten@gmail.com>
2018-09-24 21:54:20 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2", "two" => "2", "too" => "2",
+    "for" => "4", "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end 
+
+def word_substituter(tweet)
+  array_tweet = tweet.split(" ")
+  short_tweet = []
+  array_tweet.each do |word|
+    if dictionary.keys.include?(word.downcase)
+      short_tweet << dictionary[word]
+    else
+      short_tweet << word 
+    end 
+  end 
+  short_tweet.join(" ")
+end 
+
+def bulk_tweet_shortener(array_of_tweets)
+  array_of_tweets.map do |tweet|
+    puts word_substituter(tweet)
+  end 
+end 
